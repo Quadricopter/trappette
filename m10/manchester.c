9717ed35cc6f1878894a348c13f73001ebe0ad86
Michael Leyssenne <quadricopter@gmail.com>
2017-08-11 09:59:16 +0200

    Cleaning: Remove useless include

--- m10/manchester.c
+++ m10/manchester.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <getopt.h>
-#include <signal.h>
 
 #include "manchester.h"
 #include "tsip.h"
